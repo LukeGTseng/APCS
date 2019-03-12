3f6a0f0264364df5386c9b8ffcd9a8e0585cf237
Luke Tseng <lukegtseng@gmail.com>
2019-03-12 13:31:30 -0700

    Create Exercise_2.java

---
+++ Unit_1/Exercise_2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class name {
+	public static void main(String[] args ) {
+		String str;
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter your name: ");
+		str = s.next();
+		System.out.println("My name is " + str + " and I love Java!!");
+	}
+}
