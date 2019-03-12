ba9c43752408dbfb8b476499da560ae0955336ec
Luke Tseng <lukegtseng@gmail.com>
2019-03-12 12:59:58 -0700

    Create Car.java

---
+++ vehicles/Car.java
@@ -0,0 +1,37 @@
+package vehicles;
+
+public class Car extends Vehicle {
+	static int increment = 1;
+
+	public Car() {
+		super("C" + increment);
+		increment += 2;
+	}
+
+	public static Car readFromInput() {
+		Car newCar = new Car();
+		numOfVehicles++;
+		return newCar;
+	}
+
+	void open() {
+		System.out.println("Trunk Open");
+	}
+
+	void close() {
+		System.out.println("Trunk Closed");
+	}
+
+	void getSpeed() {
+		System.out.println("Speed is at " + speed);
+	}
+
+	void getGasMileage() {
+		System.out.println("The gas mileage is 25 miles per gallon");
+	}
+
+	void getWeight() {
+		System.out.println("Weighs 2000 pounds.");
+		
+	}
+}
