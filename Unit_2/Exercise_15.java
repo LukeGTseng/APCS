2dcf29032e2a2468cc9ac2c9acfa4972903f325e
Luke Tseng <lukegtseng@gmail.com>
2019-03-12 13:59:59 -0700

    Create Exercise_15.java

---
+++ Unit_2/Exercise_15.java
@@ -0,0 +1,19 @@
+package Unit_2;
+public class Exercise_15 {
+	public static void main(String[] args) {
+		float fl = 1.5f;
+		int i = 102;
+		double d = 3.14; 
+		char c = 'a';
+		long l = 100;
+		short s = 4;
+		System.out.println((double) i);
+		System.out.println((int) d);
+		System.out.println((int) c);
+		System.out.println((char) i);
+		System.out.println((short) l);
+		System.out.println((long) s);
+		System.out.println((double) fl);
+		System.out.println((int) fl);
+	}
+}
