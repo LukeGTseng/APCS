655a1870c708f09e043da6892ddfd00882eba79e
Luke Tseng <lukegtseng@gmail.com>
2019-03-12 13:41:12 -0700

    Create Exercise_16.java

---
+++ Unit_1/Exercise_16.java
@@ -0,0 +1,12 @@
+package Unit_1;
+
+public class Exercise_16.java {
+	public static void main(String[] args) {
+	for(int i = 1; i<=5;i++ ) {
+		for(int j = 1; j<=5;j++) {
+			System.out.print(i*j + "\t"); //prints out the multiplication of i and j which gives us a multiplication table
+		}
+		System.out.println();
+	}
+	}
+}
