f96bd158c2791dba943f8c60cc6f58cf1325b4e0
Luke Tseng <lukegtseng@gmail.com>
2019-03-12 13:03:15 -0700

    Create Employee.java

---
+++ arrayList/Employee.java
@@ -0,0 +1,29 @@
+package arrayLists;
+
+import java.util.*;
+
+public class Employee {
+	private String id;
+	private String name;
+	private String birthday;
+	ArrayList<String> skills = new ArrayList<String>();
+	ArrayList<String> projects = new ArrayList<String>();
+	
+	public static Employee readFromFile(String employee) {
+		Scanner Pog = new Scanner(employee).useDelimiter(";");
+		Employee JimJPoggers = new Employee();
+		JimJPoggers.id = Pog.next();
+		JimJPoggers.name = Pog.next();
+		JimJPoggers.birthday = Pog.next();
+		Scanner skillz = new Scanner(Pog.next()).useDelimiter(",");
+		while(skillz.hasNext()) {
+			JimJPoggers.skills.add(skillz.next());
+		}
+		Scanner projectz = new Scanner(Pog.next()).useDelimiter(",");
+		while(projectz.hasNext()) {
+			JimJPoggers.projects.add(projectz.next());
+		}
+		return JimJPoggers;
+	}
+	
+}
