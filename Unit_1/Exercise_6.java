41c85e93d650b1e70ddeaae50e83bd7a5b7e68fa
Luke Tseng <lukegtseng@gmail.com>
2019-03-12 13:33:19 -0700

    Create Exercise_6.java

---
+++ Unit_1/Exercise_6.java
@@ -0,0 +1,20 @@
+package Unit_1;
+
+public class Exercise_6 {
+	
+	public static void name() {
+		System.out.println("Luke Tseng");		
+	}
+	public static void address() {
+		System.out.println("3810 142nd Pl NE Bellevue, WA 98007");		
+	}
+	public static void studentid() {
+		System.out.println("131276");		
+	}
+	
+	public static void main(String[] args) {
+		address(); //To send me a letter
+		studentid(); //To check out a book at the library
+		name(); //To find my school transcript
+	}
+}
