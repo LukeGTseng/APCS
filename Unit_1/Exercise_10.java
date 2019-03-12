5d1e54660e906a4fc90a44880a190175087adb0f
Luke Tseng <lukegtseng@gmail.com>
2019-03-12 13:37:26 -0700

    Create Exercise_10.java

---
+++ Unit_1/Exercise_10.java
@@ -0,0 +1,16 @@
+package Unit_1
+
+import java.util.Scanner;
+
+public class Exercise_10 {
+    public static void main(String[] args) 
+    {
+        double celsius, fahrenheit;
+
+        System.out.println("Enter temperature in Fahrenheit:");
+        Scanner s = new Scanner(System.in);
+        fahrenheit = s.nextDouble();
+        celsius = (fahrenheit-32)*5/9;
+        System.out.println("Temperature in Celsius:" + celsius);        
+    }
+}
