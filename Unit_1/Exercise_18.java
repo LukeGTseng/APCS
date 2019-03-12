a4a7b126758898276aa7d1034ebe4d177e683469
Luke Tseng <lukegtseng@gmail.com>
2019-03-12 13:42:23 -0700

    Create Exercise_18.java

---
+++ Unit_1/Exercise_18.java
@@ -0,0 +1,12 @@
+package Unit_1;
+
+public class Exercise_18 {
+	public static void main(String[] args) {
+		for(int i = 1; i<=5;i++) {
+			for(int j = 1;j<=i;j++) { //prints out how many values i has
+				System.out.print(i); //prints out i
+			}
+			System.out.println();
+		}
+	}	
+}
