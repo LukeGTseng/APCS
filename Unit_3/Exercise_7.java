a7310cb1deaa10caab3afbb73afea4b662bb4779
Luke Tseng <lukegtseng@gmail.com>
2019-03-13 08:57:56 -0700

    Update Exercise_7.java

--- Unit_3/Exercise_7.java
+++ Unit_3/Exercise_7.java
@@ -1,6 +1,6 @@
 package Unit_3;
 import java.util.*;
-public class Exercises {
+public class Exercise_7 {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int one = 0;
