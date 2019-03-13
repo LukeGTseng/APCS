c702a9ed039f6aaf653b0087f98eec8bd1956990
LukeGTseng <lukegtseng@gmail.com>
2019-03-13 15:56:21 -0700

    Update SongCollection.java

--- Projects/GazillionSongs/SongCollection.java
+++ Projects/GazillionSongs/SongCollection.java
@@ -5,4 +5,16 @@ public class SongCollection {
 	public SongCollection(ArrayList<Song> songs) {
 		
 	}
+	public void filterYear(Range r) {
+		
+	}
+	public void filterRank(Range r) {
+		
+	}
+	public void filterArtist(Range r) {
+		
+	}
+	public void filterTitle(Range r) {
+		
+	}
 }
