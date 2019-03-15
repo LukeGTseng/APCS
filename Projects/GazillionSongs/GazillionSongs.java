10086f4d2df3950471aa0430245501895c56972b
Luke Tseng <lukegtseng@gmail.com>
2019-03-15 09:47:28 -0700

    Update GazillionSongs.java

--- Projects/GazillionSongs/GazillionSongs.java
+++ Projects/GazillionSongs/GazillionSongs.java
@@ -1,7 +1,19 @@
 package GazillionSongs;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class GazillionSongs {
 	public static void main(String[] args) {
+		Scanner ipt = new Scanner(System.in);
+		System.out.println("Which file would you like to input?");
+		File iptFile = new File(ipt.nextLine());
+		Scanner f = new Scanner(iptFile);
+		System.out.println("Which file would you like to output to?");
+		File optFile = new File(ipt.nextLine());
 		
+		while() {
+			
+		}
 	}
 }
