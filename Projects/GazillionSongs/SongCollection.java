de7407a0134d269ab50aaced32a4e6b0f90246a7
Luke Tseng <lukegtseng@gmail.com>
2019-03-15 09:47:52 -0700

    Update SongCollection.java

--- Projects/GazillionSongs/SongCollection.java
+++ Projects/GazillionSongs/SongCollection.java
@@ -1,35 +1,58 @@
 package GazillionSongs;
 
 import java.util.*;
+
 public class SongCollection {
 	private ArrayList<Song> songs = new ArrayList<>();
-	
+
 	public SongCollection(ArrayList<Song> songs) {
-		
+		this.songs = songs;
 	}
 	public void filterYear(Range r) {
-		if()
+		for (int i = 0; i < songs.size(); i++) {
+			if (r.contains(songs.get(i).getYear()) == false) {
+				songs.remove(i);
+			}
+		}
 	}
 	public void filterRank(Range r) {
-		
+		for (int i = 0; i < songs.size(); i++) {
+			if (r.contains(songs.get(i).getYear()) == false) {
+				songs.remove(i);
+			}
+		}
 	}
 	public void filterArtist(String s) {
-		
+		for (int i = 0; i < songs.size(); i++) {
+			if (!songs.get(i).getArtist().contains(s)) {
+				songs.remove(i);
+			}
+		}
 	}
 	public void filterTitle(String s) {
-		
+		for (int i = 0; i < songs.size(); i++) {
+			if (!songs.get(i).getTitle().contains(s)) {
+				songs.remove(i);
+			}
+		}
 	}
 	public void sortYear() {
-		
+		int[] year = new int[songs.size()];
+		for(int i = 0;i < year.length;i++) {
+			year[i] = songs.get(i).getYear();
+		}
 	}
 	public void sortRank() {
-		
+		int[] rank = new int[songs.size()];
+		for(int i = 0;i < rank.length;i++) {
+			rank[i] = songs.get(i).getRank();
+		}
 	}
 	public void sortArtist() {
-		
+
 	}
 	public void sortTitle() {
-		
+
 	}
 	public static void mergeNum(int arr[], int left, int middle, int right) {
 		int n1 = middle - left + 1;
@@ -75,6 +98,7 @@ public class SongCollection {
 			mergeNum(arr, left, middle, right);
 		}
 	}
+
 	public static void mergeStr(String arr[], int left, int middle, int right) {
 		int n1 = middle - left + 1;
 		int n2 = right - middle;
@@ -90,7 +114,7 @@ public class SongCollection {
 		int i = 0, j = 0;
 		int k = left;
 		while (i < n1 && j < n2) {
-			if (L[i].compareTo(R[j])<=0) {
+			if (L[i].compareTo(R[j]) <= 0) {
 				arr[k] = L[i];
 				i++;
 			} else {
