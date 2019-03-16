f2972b08b7fadbd39214f26cc6a0db0541646efc
Luke Tseng <lukegtseng@gmail.com>
2019-03-15 22:34:06 -0700

    Update Song.java

--- Projects/GazillionSongs/Song.java
+++ Projects/GazillionSongs/Song.java
@@ -2,24 +2,24 @@ package GazillionSongs;
 
 import java.util.*;
 public class Song {
-	private String year;
-	private String rank;
+	private int year;
+	private int rank;
 	private String artist;
 	private String title;
 	
 	public static Song parse(String s) {
 		Scanner scn = new Scanner(s).useDelimiter("\t");
 		Song song = new Song();
-		song.year = scn.next();
-		song.rank = scn.next();
+		song.year = scn.nextInt();
+		song.rank = scn.nextInt();
 		song.artist = scn.next();
 		song.title = scn.next();
 		return song;
 	}
-	public String getYear() {
+	public int getYear() {
 		return year;
 	}
-	public String rank() {
+	public int getRank() {
 		return rank;
 	}
 	public String getArtist() {
