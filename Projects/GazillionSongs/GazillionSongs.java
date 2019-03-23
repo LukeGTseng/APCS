24f8449ceeb62fa8ed950b10f19d331898afd73e
Luke Tseng <lukegtseng@gmail.com>
2019-03-22 17:11:52 -0700

    Update GazillionSongs.java

--- Projects/GazillionSongs/GazillionSongs.java
+++ Projects/GazillionSongs/GazillionSongs.java
@@ -5,7 +5,9 @@ import java.util.*;
 
 public class GazillionSongs {
 	public static void main(String[] args) throws FileNotFoundException {
-		ArrayList<Song> songs = new ArrayList<Song>();
+		ArrayList<Song> tempList = new ArrayList<Song>();
+		SongCollection songs = new SongCollection(tempList);
+		
 		Scanner ipt = new Scanner(System.in);
 		System.out.println("Which file would you like to input?"); //C:\Users\s-tsengl\Downloads\APCS_Java-master\Projects\GazillionSongs\agazillionsongs.txt
 		File iptFile = new File(ipt.nextLine());
@@ -13,10 +15,10 @@ public class GazillionSongs {
 		/*System.out.println("Which file would you like to output to?");
 		File optFile = new File(ipt.nextLine());*/
 		while(f.hasNextLine()) {
-			songs.add(Song.parse(f.nextLine()));
+			tempList.add(Song.parse(f.nextLine()));
 		}
-		for(int i = 0; i<songs.size();i++) {
-			System.out.println(songs.get(i).getYear());
+		for(int i = 0; i<tempList.size();i++) {
+			songs.filterYear();
 		}
 		
 	}
